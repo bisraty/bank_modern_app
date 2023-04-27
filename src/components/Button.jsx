3bc47ce41d2446bcec0452088b0f88111998d326
bisraty <bisrat2007manu.com>
2023-04-27 16:39:21 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+}
